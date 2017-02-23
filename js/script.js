fa923f899ac737387a5d6b9aeeef5ee1c26d4b1f
FayeZ <im5dii@gmail.com>
2017-02-22 20:42:24 -0800

    v1.5

--- js/script.js
+++ js/script.js
@@ -10,31 +10,6 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 });
 
 
-// Get the modal
-var modal = document.getElementById('myModal');
-
-var images = document.getElementsByClassName("imageClass");
-for(var i = 0; i < images.length; i++){
-    var img = images[i];
-    var modalImg = document.getElementById("img");
-    var captionText = document.getElementById("caption");
-    img.onclick = function(){
-        modal.style.display = "block";
-        modalImg.src = this.src;
-        modalImg.alt = this.alt;
-        captionText.innerHTML = this.alt;
-    }
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
-}
-
-
 // To Top Button
 $(document).ready(function(){
   
